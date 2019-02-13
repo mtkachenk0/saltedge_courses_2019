d27f04186449e6672e9b15638da2ee3e9b6fde3c
Alex <pogoretschi.alexandr94@gmail.com>
2019-02-13 21:50:31 +0200

    homework_lesson_3.rb

---
+++ Pogoretchi_Alex/homework_lesson_3.rb
@@ -0,0 +1,31 @@
+#Float/Integer.
+puts -123.odd? = true 
+
+puts 6893.next = 6894
+
+puts (0).pred = -1
+ 
+puts 12.to_f / 4.5.round = 2.4
+
+puts 12.to_i / 4.5.round = 2
+
+puts ((28 % 6) * 4.7).round.pred = 18
+
+#String
+puts "Brand new bicycle\r\n".chop = Brand new bicycle
+
+puts "Brand new bicycle"[6,3] = new 
+
+puts "Brand new bicycle".gsub"bicycle","" = Brand new
+
+puts "Brand new bicycle".split.last = bicycle
+
+puts "Brand new bicycle".index("w") = 8
+
+puts "I thought I knew\neverything\nbefore that night" =
+I thought I knew
+everything
+before that night
+
+puts "Final result from Float/Integer part is #{-123+6894+-1+2+18}"
+Final result from Float/Integer part is 6790
