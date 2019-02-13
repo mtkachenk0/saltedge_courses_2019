7dcb07266550566d3289ea0ced7e5da8c4ac4c34
Natalia Macarova <natalia.carasiova@gmail.com>
2019-02-14 00:04:18 +0200

    homework 3

---
+++ macarova/homework_lesson_3.rb
@@ -0,0 +1,12 @@
+puts -123.odd?
+6893.next
+0.pred
+(12.to_f / 4.5).ceil
+((28 % 6) * 4.7).floor
+"Brand new bicycle\r\n".chomp
+"Brand new bicycle"[6..8]
+"Brand new bicycle".sub("bicycle", "")
+"Brand new bicycle".end_with?("bicycle")
+"Brand new bicycle".index('w')
+"I thought I knew everything before that night".partition("everything")
+"Final result from Float/Integer part is:#{-123+6894+-1+3+18}"
