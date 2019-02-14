1acdb71eee46a88ba5179a2a7b88eafdc9dae5c4
Liliana Tataru <lilianat@saltedge.com>
2019-02-14 18:42:08 +0200

    Update round up&down

--- lilianat/lesson3/homework.rb
+++ lilianat/lesson3/homework.rb
@@ -1,8 +1,8 @@
 puts -123.odd?
 puts 6893.next
 puts 0.pred
-puts (12.to_f / 4.5).round
-puts ((28 % 6) * 4.7).round
+puts (12.to_f / 4.5).ceil
+puts ((28 % 6) * 4.7).floor
 puts "Brand new bicycle\r\n".strip
 puts "Brand new bicycle"[6..8]
 puts "Brand new bicycle"[/(.*)\s/,1]
