aa1e03cfaaf51684373eba20fdd0e1bb900c79b5
jenea_mocan <jeneamocan@gmail.com>
2019-02-14 12:11:01 +0200

    editing

--- homework_lesson_3.rb
+++ homework_lesson_3.rb
@@ -2,3 +2,11 @@ puts -123.odd?
 puts 6893+1
 puts 0-1
 puts (12.to_f/4.5).round
+puts ((28%6)*4.7).floor
+puts "Brand new bicycle\\r\\n"
+puts "Brand new bicycle"[6..8]
+puts "Brand new bicycle".split(' ')[0...-1].join(' ')
+puts "Brand new bicycle".end_with?("bicycle")
+puts "Brand new bicycle".index("w")
+puts "I thought I knew everything before that night".split(/(?=everything)|(?<=everything )/)
+puts "Final result from Float/Integer part is: #{(6893+1)+(0-1)+((12.to_f/4.5).round)+(((28%6)*4.7).floor)}"
