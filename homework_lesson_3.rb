2d0740b402bd7e11c139e9a04484b55d8cefc764
jenea_mocan <jeneamocan@gmail.com>
2019-02-14 16:04:21 +0200

    using partition method

--- homework_lesson_3.rb
+++ homework_lesson_3.rb
@@ -8,5 +8,5 @@ puts "Brand new bicycle"[6..8]
 puts "Brand new bicycle".split(' ')[0...-1].join(' ')
 puts "Brand new bicycle".end_with?("bicycle")
 puts "Brand new bicycle".index("w")
-puts "I thought I knew everything before that night".split(/(?=everything)|(?<=everything )/)
+puts "I thought I knew everything before that night".partition("everything ")
 puts "Final result from Float/Integer part is: #{(6893.next)+(0.pred)+((12.to_f/4.5).round)+(((28%6)*4.7).floor)}"
