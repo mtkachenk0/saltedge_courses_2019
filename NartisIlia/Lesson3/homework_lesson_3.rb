d97be7f82856c658a295fd0f016cd9cd6c9fbfad
william <william.nartis@gmail.com>
2019-02-14 16:24:57 +0200

    Home work #2

---
+++ NartisIlia/Lesson3/homework_lesson_3.rb
@@ -0,0 +1,32 @@
+# Float/Integer
+
+puts (((-123%2).zero?) ? "чёт":"нечёт")         # Exercise 1
+
+puts 6893.next                                  # Exercise 2
+
+puts 0.pred                                     # Exercise 3
+
+puts (12.to_f/4.5).round(2)                     # Exercise 4
+
+puts ((26%6)*4.7).floor                         # Exercise 5
+
+
+
+# String
+
+puts "Brand new bicycle\r\n".chomp                          # Exercise 1
+
+puts "Brand new bicycle" [6..8]                             # Exercise 2
+
+puts "Brand new bicycle" [0..-8]                            # Exercise 3 var1
+
+puts "Brand new bicycle".sub("Brand new bicycle".split.last,"")  # Exercise 3 var2
+
+puts "Brand new bicycle".split.last.include?("bicycle")     # Exercise 4
+
+puts "Brand new bicycle".index('w')                          # Exercise 5
+
+
+
+
+
