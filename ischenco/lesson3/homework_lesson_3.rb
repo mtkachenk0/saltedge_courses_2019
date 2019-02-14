d850f040b4b8ba73099e9a2658e558341da21b03
ischenco <ischenco@gmail.com>
2019-02-14 18:16:56 +0200

    new ruby file with homework lesson3

---
+++ ischenco/lesson3/homework_lesson_3.rb
@@ -0,0 +1,18 @@
+puts -123.odd?
+puts 6893.next
+puts 0.pred
+puts (12.to_f/4.5).ceil
+puts ((28%6)*4.7).floor
+puts "Brand new bicycle\r\n".chomp
+puts "Brand new bicycle"[6..8]
+puts p "Brand new bicycle"[0.."Brand new bicycle".rindex(' ')]
+puts"Brand new bicycle".end_with?("bicycle")
+puts"Brand new bicycle".index('w')
+puts "I thought I knew everything before that night".partition('')
+puts "Final result from Float/Integer part is:"+6791.to_s 
+
+
+
+
+
+
