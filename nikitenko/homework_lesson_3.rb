6a974fb63ff128d604c272fd9a16468599446324
darya1996 <dnikitenko1996@gmail.com>
2019-02-17 14:27:53 +0200

    Homework lesson 4 add

--- nikitenko/homework_lesson_3.rb
+++ nikitenko/homework_lesson_3.rb
@@ -1,7 +1,7 @@
 puts -123.odd?
 puts 6893.next
 puts 0.pred
-puts (12.to_f/4.5).round
+puts (12.to_f/4.5).ceil
 puts (28%6*4.7).floor
 puts "Brand new bicycle\r\n".chop
 puts "Brand new bicycle"[6..8]
