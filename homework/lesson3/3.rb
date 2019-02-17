bba6ed94f881ca2b967e46d8467c1eece88dc3cb
BuTaMuH <vitamin1st1982@gmail.com>
2019-02-17 18:46:55 +0200

    my homework3

---
+++ homework/lesson3/3.rb
@@ -0,0 +1,15 @@
+puts -123.odd?
+puts 6893.next
+puts 0.pred
+puts (12.to_f/4.5).ceil
+puts (12.to_f/4.5).ceil
+puts (28.modulo(6)*4.7).floor
+
+puts "Brand new bicicle\n\r".chomp
+puts "Brand new bicicle"[6..8]
+puts "Brand new bicicle"[0..9]
+puts "Brand new biccicle".end_with?"biccicle"
+puts "Brand new bicycle".index("w")
+puts "I thought I knew everything before that night".partition("everything")
+puts "Final result from Float/Integer part is: #{6894-1+3+18}"
+
