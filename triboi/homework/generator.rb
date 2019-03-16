cfa5a0279ace0e81e152bf256a60f8eb675ffa40
triboi <47428315+triboi@users.noreply.github.com>
2019-03-16 11:31:25 +0200

    Add Homework Test9
    new test9

---
+++ triboi/homework/generator.rb
@@ -0,0 +1,20 @@
+
+require 'pry'
+require 'json'
+
+data = JSON.parse(File.read("names_list.json"))
+p data
+names=[]
+surnames=[]
+data.each{|k,v| names << k and surnames << v}
+surnames.uniq!
+data_output = names.product(surnames)
+data_output.map! do |i|
+  i.join(" ")
+end
+File.open("output_list.json", "w") do |f|
+ f.write(JSON.pretty_generate(data_output))
+end
+
+
+binding.pry
