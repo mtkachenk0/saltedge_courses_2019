f77442428076570524489114aa443603815f21a4
william <william.nartis@gmail.com>
2019-03-19 13:09:10 +0200

    Home Work 12

---
+++ NartisIlia/Lesson12/H_W12.rb
@@ -0,0 +1,26 @@
+require 'pry'
+require 'rest-client'
+require 'nokogiri'
+
+data = RestClient.get('https://point.md/ru').body
+html = Nokogiri::HTML(data)
+
+big_image=html.css("div[class='middle-block'] img[src*='https://i.simpalsmedia.com/point.md/news/370x220']")
+little_image=html.css("div[class='middle-block'] img[src*='https://i.simpalsmedia.com/point.md/news/160x90']")
+
+#Get first 10 images
+top_news={big_image[0]['alt']=>big_image[0]['src']}
+i=0
+little_image.each{|v|  top_news[v['alt']]=v['src'] if (i=i+1)<10}
+
+puts top_news
+
+#write images to folder 
+top_news.each_pair do |k,v|
+ file=RestClient.get(v)
+ File.write('/home/william/courses/saltedge_courses_2019/NartisIlia/Lesson12/'+k,file)
+end
+
+
+binding.pry
+1
