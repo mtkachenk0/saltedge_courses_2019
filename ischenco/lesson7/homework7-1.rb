5b9a2339dff16e26ed0a1ceec06937ebc0eee52b
ischenco <ischenco@gmail.com>
2019-03-20 21:43:12 +0200

    cleanup

--- ischenco/lesson7/homework7-1.rb
+++
@@ -1,3 +0,0 @@
-def interval(a, b)
-  (a..b).to_a.select { |num| num % 7 == 0 && num % 5 != 0 }.join(" - ")
-end
--- ischenco/lesson7/homework7-3.rb
+++
@@ -1,6 +0,0 @@
-def numbers(num)
-  print num.chomp.split(',')
-end
-puts "Input your numbers"
-numb = gets
-numbers(numb)
