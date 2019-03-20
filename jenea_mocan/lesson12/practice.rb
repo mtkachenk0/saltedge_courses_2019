82f8f862c7463d865dc69066e917a3734880c16e
jenea_mocan <jeneamocan@gmail.com>
2019-03-20 21:37:02 +0200

    Homework lesson 9 (test)

--- jenea_mocan/lesson12/practice.rb
+++
@@ -1,8 +0,0 @@
-require "watir"
-browser = Watir::Browser.new
-browser.goto("kinopoisk.ru")
-browser.text_field(class: "header-fresh-search-partial-component__field").set("Интерстеллар")
-browser.send_keys(:enter)
-browser.a(href: "/level/1/film/258687/sr/1/").click
-actors = browser.lis(itemprop: "actors")[0..4]
-actors.map {|i| i.text}
--- jenea_mocan/lesson9/homework_with_modules/main.rb
+++
@@ -1,4 +0,0 @@
-require 'pry'
-require_relative 'bmw_board_computer'
-require_relative 'alfa_romeo_board_computer'
-
