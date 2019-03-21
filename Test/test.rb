ca55176f68b6526a1ac630a7ce4017a2a14c234d
william <william.nartis@gmail.com>
2019-03-21 13:25:17 +0200

    Test from 08.03.2019

---
+++ Test/test.rb
@@ -0,0 +1,25 @@
+require 'pry'
+require 'json'
+
+$stdout = File.open('output_file','w')
+
+json = open("Names.json").read
+
+par = JSON.parse(json)
+puts par
+arr=par.to_a
+
+i=1 # i-counter iterations
+while arr.size > i
+    i+=1  
+    arr.each_with_index do |val,index| 
+        if arr.size > index+1
+            str = val[1]
+            arr[index][1]=arr[index+1][1]
+            arr[index+1][1] = str
+        else 
+            break
+        end
+    end
+    puts arr.to_h
+end
