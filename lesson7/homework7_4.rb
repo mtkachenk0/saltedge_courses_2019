9158317ebaa9e007f6a85327d550c160ed62e893
PovolocicoDima <PovolocicoDima>
2019-03-21 13:42:58 +0200

    hw_7

---
+++ lesson7/homework7_4.rb
@@ -0,0 +1,14 @@
+# Write a program that accepts a comma separated sequence of words as input and
+# prints the words in a comma-separated sequence after sorting them alphabetically.
+# Input:
+# without,hello,bag,world
+# Output:
+# bag,hello,without,world
+puts "Write words separated with comma"
+
+def list(a)
+	list = a.split(/,/).sort
+	string = list.map{|word| word.to_s}.join(",")
+	puts string.delete(' ')
+end
+list(gets.chomp.downcase)
