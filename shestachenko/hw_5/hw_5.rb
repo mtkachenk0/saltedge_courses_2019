9c696d2ccc19dde8161681547f3f4dfe48b16f08
yurets89 <yurets89@gmail.com>
2019-03-24 01:19:56 +0200

    hw_5

--- shestachenko/hw_5/hw_5.rb
+++ shestachenko/hw_5/hw_5.rb
@@ -13,7 +13,7 @@ end
 #Задача №2
 y = 17
 if y > 0 && y <= 2
-	puts "nadbavka 0%"
+    puts "nadbavka 0%"
 elsif y > 2 && y <= 10
     puts "nadbavka 10%"
 elsif y > 10 && y <= 20
@@ -30,7 +30,7 @@ password = "b5678"
 
 b = {ivan:334455, alex:777, petr:"b5678"}
 if login == b.keys[0] && password == b.values[0] || login == b.keys[1] && password == b.values[1] || login == b.keys[2] && password == b.values[2]
-	puts "welcome"
+    puts "welcome"
 else
     puts "incorrect"
 end
