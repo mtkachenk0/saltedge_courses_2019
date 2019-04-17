dbabfdd3872d735093e7d16781b536c85abb5f37
Ion Mocan <ionatanmocan@gmail.com>
2019-04-17 20:17:40 +0300

    added homework 7

--- Mocan_Ion/lesson6/lesson6_4.rb
+++
@@ -1,8 +0,0 @@
-puts "Give me a list of words, separated only by comma"
-words = $stdin.gets.chomp
-
-def turn_string_into_array(words)
-  puts words.split(",").sort.join(",")
-end
-
-turn_string_into_array(words)
